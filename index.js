2787be56ba45df6dd68a8fbcbf02305a361bee50
JalanshMunshi <jalansh.munshi1366@gmail.com>
2020-08-08 15:22:31 +0530

    Added the post endpoint for registering user. Added prod and dev keys for DB connection. Modified gitignore.

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const User = require('./models/user');
+const config = require('./config/key');
 
-require('dotenv').config();
+app.use(express.json());
+app.use(cookieParser());
 
-const uri = process.env.ATLAS_URI;
+const uri = config.mongoURI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true  })
         .then(() => console.log('MongoDB connected.'))
         .catch((err) => console.log(`MongoDB connection failed: ${err}.`));
 
-app.get('/', (req, res) => {
-    res.send('hello world!');
-});
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+    user.save()
+    .then(() => res.status(200).json(`User '${req.body.firstname}' added!`))
+    .catch((err) => res.status(400).json(`Unable to add user. Error: ${err}.`));
+})
 
-app.listen(5000);
+port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+});
--- models/user.js
+++ models/user.js
@@ -3,15 +3,18 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     firstname: {
         type: String,
+        required: true,
         maxlength: 50
     },
     email: {
         type: String,
+        required: true,
         trim: true,
         unique: 1
     },
     password: {
         type: String,
+        required: true,
         minlength: 5
     },
     lastname: {
