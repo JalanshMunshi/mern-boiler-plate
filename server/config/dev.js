56c7b65a9bd61bb721c19d5f3f82f6f8d283eba8
JalanshMunshi <jalansh.munshi1366@gmail.com>
2020-08-10 19:40:15 +0530

    Initialized Heroku deployment. Modified package.json.

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://jalansh:helloworld@cluster-online-shop.wz8uh.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
--- server/index.js
+++ server/index.js
@@ -86,7 +86,7 @@ app.get('/api/user/logout', auth, (req, res) => {
     });
 });
 
-port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
